3be6ce7c738a256287a286b73ebf6bb3fd920d84
Rehan Bari <rbari921@mtroyal.ca>
2022-05-09 19:50:30 -0600

    OOP Practice

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import turtle
+from turtle import Turtle, Screen
 
-timmy = turtle.Turtle()
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("green")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
