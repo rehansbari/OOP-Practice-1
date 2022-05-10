2a2d55d9d90683b3369440f8092f86f0857f7cc5
Rehan Bari <rbari921@mtroyal.ca>
2022-05-09 20:09:58 -0600

    OOP Practice

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import turtle
 from turtle import Turtle, Screen
+import prettytable
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("green")
-timmy.forward(100)
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("green")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+table = prettytable.PrettyTable()
+
+table.add_column("Pokemon",
+                 ["Pikachu", "Squirtle"])
+table.add_column("Type",
+                 ["Lightning", "Water"])
+table.align = "l"
+print(table)
